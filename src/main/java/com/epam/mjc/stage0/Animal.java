f50da79c4193409a04b199edb6f3ef043a785066
Marcos Soto <you@example.com>
2024-04-21 23:57:15 -0600

    Final task 6 1

--- src/main/java/com/epam/mjc/stage0/Animal.java
+++ src/main/java/com/epam/mjc/stage0/Animal.java
@@ -1,5 +1,23 @@
 package com.epam.mjc.stage0;
 
 public class Animal {
-
+	private String color;
+	private int numberOfPaws;
+	private boolean hasFur;
+	
+	
+	public Animal(String color, int numberOfPaws, boolean hasFour) {
+		this.color = color;
+		this.numberOfPaws = numberOfPaws;
+		this.hasFur = hasFour;
+	}
+	
+	
+	
+	   public String getDescription() {
+	        String furDescription = hasFur ? "a" : "no";
+	        String pawDescription = numberOfPaws == 1 ? "paw" : "paws";
+	        return "This animal is mostly " + color + ". It has " + numberOfPaws + " " + pawDescription + " and " + furDescription + " fur.";
+	    }
+	
 }
--- src/main/java/com/epam/mjc/stage0/Dog.java
+++ src/main/java/com/epam/mjc/stage0/Dog.java
@@ -1,5 +1,12 @@
 package com.epam.mjc.stage0;
 
-public class Dog {
-
+public class Dog extends Animal {
+	public Dog() {
+        super("brown", 4, true);
+    }
+	
+	@Override
+    public String getDescription() {
+        return super.getDescription() + "This animal is mostly brown. It has 4 paws and a fur.";
+    }
 }
